4a46324f9438fb1fdf9dc34f746a1422f06d7b10
Juan Camilo Ortiz Villegas <jcortiz@personalsoft.com.co>
2021-02-20 02:21:04 -0500

    Prueba de modificación en dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Alquilandoando
+                    Alquilandoando__
                 </div>
 
                 <div class="links">
