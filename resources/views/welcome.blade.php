8cd23810b0de7466a96dfbf19cb3cbf1b0173d56
Juan Camilo Ortiz Villegas <jcortiz@personalsoft.com.co>
2021-02-20 02:00:54 -0500

    Se agrega  titulo de la web en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Alquilandoando
                 </div>
 
                 <div class="links">
